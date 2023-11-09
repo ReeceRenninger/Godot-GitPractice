1f83b93168a614b776db9ae495937138115094f3
ReeceRenninger <renningerreece@gmail.com>
2023-11-09 14:18:49 -0800

    player movement working using arrow keys or WASD

--- player.gd
+++ player.gd
@@ -9,5 +9,26 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# check for input, move in the given direction, play the appropriate animation
 func _process(delta):
-	pass
+	# zero out our velocity first
+	var velocity =  Vector2.ZERO # player movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed # normalizes diagonal movement so the speeds aren't doubled together from 2 key presses
+	# $ is shorthand for get_node(), so the below code is the same as get_node("AnimatedSprite2D").play()
+	# $ in GDScript reutrns the node at the relative path form the CURRENT node or reutrns null if node is not found
+		$AnimatedSprite2D.play() # if player is moving, play animation
+	else:
+		$AnimatedSprite2D.stop() # if player is NOT moving, stop animation
+		
+	# clamping to prevent the sprite from exiting screen
+	position += velocity * delta # delta here refers to the frame length, the amount of time that the previous frame took to complete, keeps movement consistent even in frame changes
+	position = position.clamp(Vector2.ZERO, screen_size) # global variable of screen size used to keep spirte in bounds
